ec7c404f2b297150dd217ab19de243e19ff4c7bc
DennyBui <dbuidmd2018@gmail.com>
2023-06-17 07:08:25 -0700

    lab complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,42 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const displayTitles = movies.map((movie) => {
+      return <div key={movie.title}>{movie.title}{movie.time} </div>
+
+  })
+
+  const movieGenre = movies.map((movie,index) => {
+    return <li key={index}>{movie.genres}</li>
+  })
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+        {displayTitles}
+    <ul>
+        {movieGenre}
+    </ul>
+  </div>
+  )
 }
 
 export default Movies;
+// const movies = [
+//   {
+//     title: "Doctor Strange",
+//     time: 115,
+//     genres: ["Action", "Adventure", "Fantasy"],
+//   },
+//   {
+//     title: "Trolls",
+//     time: 92,
+//     genres: ["Animation", "Adventure", "Comedy", "Family", "Fantasy"],
+//   },
+//   {
+//     title: "Jack Reacher: Never Go Back",
+//     time: 118,
+//     genres: ["Action", "Adventure", "Crime", "Mystery", "Thriller"],
+//   },
+// ];
